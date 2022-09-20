3c34b5f195c3b20222c34ca79bad8ac8302b5f85
Van Julius Leander G. Lopez <vjlglopez@gmail.com>
2022-09-20 08:11:47 +0000

    Removed the doc strings of this python file

--- Exer_I.py
+++ Exer_I.py
@@ -1,34 +1,4 @@
 def count_and_print(n):
-    """
-    Count from 1 to n whilst displaying a message
-    
-    Count from 1 to an integer n (inclusive). For each count, print the 
-    counter value and a message separated by a single space. If the counter is
-    even, the message should be `fizz`, `fuzz` if divisible by 3 and `foo` 
-    (only) if divisible by 6.
-    
-    Parameters
-    ----------
-    n : integer
-        Count up to this number (inclusive)
-        
-    Examples
-    --------
-    >>> count_and_print(12)
-    1
-    2 fizz
-    3 fuzz
-    4 fizz
-    5
-    6 foo
-    7 
-    8 fizz
-    9 fuzz
-    10 fizz
-    11
-    12 foo
-
-    """
     for i in range(1, n+1):
         if i % 6 == 0:
             print(str(i) + " foo")
@@ -39,13 +9,8 @@ def count_and_print(n):
         else:
             print(i)
 
-        
+
 def overlap_interval(start1, end1, start2, end2):
-    """
-    This function will create 3 different lists according to the input
-    and you can read list 3 as getting the values in list1 if
-    the values are also in list2 (intersection).
-    """
     list1 = list(range(int(start1),int(end1)+1))
     list2 = list(range(int(start2),int(end2)+1))
     list3 = [values for values in list1 if values in list2]
@@ -57,28 +22,12 @@ def overlap_interval(start1, end1, start2, end2):
         
 
 def factorial(n):
-    """
-    We first have to create a function for factorial since
-    the Maclaurin's approximation utilizes it in its denominator
-    """
     x = 1
     for i in range(1, n + 1):
         x *= i
     return x
 
 def cosine(theta):
-    """Return cosine of theta using Maclaurin's approximation
-    
-    Parameters
-    ----------
-    theta : float
-        Value to compute cosine of, in radians
-        
-    Returns
-    -------
-    cos_theta : float
-        Cosine of theta
-    """
     n = 0
     value = 1 
     sum_mac = 0 
@@ -92,10 +41,6 @@ def cosine(theta):
 
 
 def gcd(a, b):
-    """
-    This function will print the gcd of two numbers
-    by getting the number at which both remainder for a and b is zero
-    """
     i = 1
     val = 0
     while(i <= a and i <= b):
@@ -106,10 +51,6 @@ def gcd(a, b):
 
 
 def biased_sum(*args, base = 2):
-    """
-    This function will take the summation of a given list
-    and double the number in the list if the remainder is zero
-    """
     if len(args) == 0:
         print('None')
     
@@ -124,11 +65,6 @@ def biased_sum(*args, base = 2):
 
 
 def last_in_sequence(digits):
-    """
-    This function will count starting from 0 when inputed with
-    a string of numbers and store it in a list as an integer.
-    It will then output the last number from the stored list
-    """
     if '0' not in digits:
         return None    
     else:
@@ -142,15 +78,6 @@ def last_in_sequence(digits):
 
 
 def check_password(pwd):
-    """
-    This function will prove below password requirements:
-    - at least 8 characters in length
-    - contains at least one lowercase letter
-    - contains at least one uppercase letter
-    - contains at least one number
-    and reject as False if the requirements are not met
-    using string methods
-    """
     if len(pwd) < 8:
         return False
     else:
@@ -173,10 +100,6 @@ def check_password(pwd):
 
 
 def is_palindrome(text):
-    """
-    This function will first clean the input strings and
-    flip the it by putting a negative value on the step
-    """
     if len(text) == 0:
         return False
     else:
@@ -189,11 +112,6 @@ def is_palindrome(text):
 
 
 def create_squares(num_stars):
-    """
-    This function will print out a series of +, - and |
-    that corresponds to the 
-    desired output using string multiplication and string concatenation.
-    """
     fixed_line = ("+ " + "- " * num_stars) * 2 + "+" + '\n'
     row_star_first = ""
     row_space_first = ""
@@ -208,12 +126,6 @@ def create_squares(num_stars):
 
 
 def create_grid(dim_block, num_stars):
-    """
-    This function will print out a series of +, - and | 
-    that corresponds to the desired output using string multiplication
-    and string concatenation. The difference between create_grid and
-    create_squares is the use of dimension block
-    """
     line = ("+ " + "- " * num_stars) * dim_block + "+" + '\n'
     line1 = ("+ " + "- " * num_stars) * dim_block + "+ " + "- " + "+" + '\n'
     row1 = ""
