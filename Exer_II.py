dd87828d6356cb2ae61c75f70b7da26b6925a757
vjlglopez <112041267+vjlglopez@users.noreply.github.com>
2022-09-21 23:44:20 +0800

    Update Exer_II.py
    Removed Docstrings on Exer_II.py

--- Exer_II.py
+++ Exer_II.py
@@ -1,18 +1,9 @@
 def chop(a_list):
-    """
-    This function will remove the first and last elements
-    in the list compared to slicing that just edits the list
-    but does not modify the succeeding outputs
-    """
     a = a_list.pop(0) and a_list.pop(-1)
     return None
 
 
 def sum_multiples(a_list):
-    """
-    This function will compute for the sum if
-    the number provided is a multiple of 3 or 5
-    """
     i = 0
     x = []
     for i in a_list:
@@ -24,17 +15,11 @@ def sum_multiples(a_list):
 
 
 def rotate(numbers, k):
-    """
-    Rotate numbers by k elements
-    """
     rot_right = numbers[-k:] + numbers[:-k]
     return rot_right
 
 
 def on_all(func, a_list):
-    """
-    Apply func to every element of a list
-    """
     mod_list = []
     for number in a_list:
           mod_list.append(func(number))
@@ -42,10 +27,6 @@ def on_all(func, a_list):
 
 
 def matrix_times_vector(mat, vec):
-    """
-    This function will perform matrix multiplication assuming 
-    that the input dimensions are always compatible
-    """
     prod_mat = []
     
     for ind_mat in mat:
@@ -60,20 +41,6 @@ def matrix_times_vector(mat, vec):
 
 
 def coder(text, to_morse=True):
-    """Convert English text to Morse code and vice versa
-    
-    Parameters
-    ----------
-    text : str
-        English text or Morse code text
-    to_morse : boolean
-        Converts text to Morse code if `True`, to English otherwise
-        
-    Returns
-    -------
-    coded_text : str
-        Text converted to Morse code or English
-    """
     morse = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -110,15 +77,6 @@ def coder(text, to_morse=True):
 
 
 def sort_by_key(items_with_keys, ascending=True):
-    """Sort items_with_keys based on the keys then by item for same keys
-    
-    Parameters
-    ----------
-    items_with_keys : list of tuples
-        list of (item, key) tuples to be sorted
-    ascending : bool
-        Sort in ascending order if True, in descending order otherwise
-    """
     if ascending == True:
         sorted_list = sorted(items_with_keys, key = lambda x:x[1])
         return sorted_list
@@ -129,10 +87,6 @@ def sort_by_key(items_with_keys, ascending=True):
 
 
 def count_words(text):
-    """
-    This function will count the occurence of a provided string
-    within the input group of words
-    """
     txt_low = text.lower()
     txt_list = txt_low.split()
     txt_list.sort()
@@ -143,10 +97,6 @@ def count_words(text):
 
 
 def display_tree(dictionary, indent=''):
-    """
-    This function will call itself if the given parameters are met
-    to continue the for-loop. The concept of recursion is used.
-    """
     cleaned = sorted(dictionary.items()) 
     
     tree = '' 
@@ -163,10 +113,6 @@ def display_tree(dictionary, indent=''):
 
 
 def get_nested_key_value(nested_dict, key):
-    """
-    This function accepts a nested dictionary nested_dict and a string key,
-    which corresponds to the key at each level joined by a period (.). 
-    """
     listed_key = key.split('.')
     
     if type(nested_dict) is not dict:
